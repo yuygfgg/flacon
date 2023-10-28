d2d718a1339ec324a1ddfd6be68ce75c572648ee
Alexander Sokolov <sokoloff.a@gmail.com>
2023-10-28 18:54:18 +0300

    test 9

--- .github/workflows/build-appimage.sh
+++ .github/workflows/build-appimage.sh
@@ -3,31 +3,18 @@
 set -e
 set -x
 
-echo ${PROGRAMS}
-
-uname -a
-echo "░░ Set env ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
+PROGRAMS="alacenc faac flac lame mac oggenc opusenc sox ttaenc wavpack wvunpack"
 export "RELEASE_DATE=$(date +%Y.%m.%d_%H.%M.%S)"
 export "RELEASE_VERSION=${GITHUB_REF#refs/*/}"
-
-if [[ "${GITHUB_REF_TYPE}"  = "tag" ]]; then
-  export APPIMAGE_NAME="flacon-${GITHUB_REF_NAME:1}-x86_64.AppImage"
-else 
-  [[ "${GITHUB_REF_TYPE}" != "tag" ]] && export APPIMAGE_NAME="flacon-$(date +%Y.%m.%d_%H.%M.%S)-x86_64.AppImage"
-fi 
-
 echo "░░ Set env ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
-pwd
-echo "░░ Set env ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
-ls -l 
-echo "░░ Set env ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
-echo "==============================="
+echo "░░ * APPIMAGE_NAME: ${APPIMAGE_NAME}"
+echo "░░ * PROGRAMS:      ${PROGRAMS}"
+echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
 export
-echo "-------------------------------"
-cat $GITHUB_ENV
-echo "==============================="
+echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
 
-echo "░░ Install packages Set env ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
+
+echo "░░ Install packages ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
 echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
 sudo apt-get -y update
 sudo apt-get -y install locales
@@ -37,8 +24,8 @@ sudo apt-get -y install flac vorbis-tools wavpack lame faac opus-tools sox
 sudo apt-get -y install desktop-file-utils
 
 echo "░░ Build Flacon ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
-cmake -E make_directory ${{github.workspace}}/build
-cmake -E make_directory ${{github.workspace}}/build/app
-cmake -DCMAKE_INSTALL_PREFIX=${{github.workspace}}/build/app/usr -DAPPIMAGE_BUNDLE=Yes -DCMAKE_BUILD_TYPE=$BUILD_TYPE $GITHUB_WORKSPACE -B ${{github.workspace}}/build
-make -C ${{github.workspace}}/build -j 8
-make -C ${{github.workspace}}/build install
+cmake -E make_directory build
+cmake -E make_directory build/app
+cmake -DCMAKE_INSTALL_PREFIX=build/app/usr -DAPPIMAGE_BUNDLE=Yes -DCMAKE_BUILD_TYPE=$BUILD_TYPE $GITHUB_WORKSPACE -B build
+make -C build -j 8
+make -C build install
